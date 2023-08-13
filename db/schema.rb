c3496cb23ba2e3c9b9d08e01ae59e04a83d0467f
hariom patidar <hariompatidar32498.com>
2023-08-13 21:05:55 +0530

    Commit message describing your changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_163059) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_152334) do
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.string "auther"
@@ -23,6 +23,42 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_163059) do
   create_table "books_customers", id: false, force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "book_id", null: false
+  end
+
+  create_table "demos", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "date_of_birth"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "adress"
+    t.string "email"
+    t.string "bio"
+    t.boolean "terms_of_service"
+    t.string "registration_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "point"
+    t.integer "games_played"
+  end
+
+  create_table "person_demos", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "number"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ test/models/person_demo_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PersonDemoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
